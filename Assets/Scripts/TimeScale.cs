f2402e5fb9dd432597ff02915408af8649c71abc
J21TheSender <jrogers4@my.athens.edu>
2015-07-04 10:32:28 -0500

    Added 7 Scenes to Tutorial
    Added some more stuff to the tutorial.

--- Assets/Scripts/TimeScale.cs
+++ Assets/Scripts/TimeScale.cs
@@ -1,41 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TimeScale : MonoBehaviour 
-{
-	private float maxFFW = 64.0f;
-	private float saveTimeScale = 1.0f;
+public class TimeScale : MonoBehaviour {
+	private static float saveTimeScale;
 
-	public void Start()
-	{ 
-		Time.timeScale = 0; 
+	public void Start() {
+		Time.timeScale = 0;
 	}
 
-	public void PlayButton()
-	{
+	public void PlayButton() {
+		//Set TimeScale to 1, regardless of current state
 		Time.timeScale = 1;
 	}
 
-	public void PauseButton()
-	{ 
-		if (Time.timeScale > 0)
-		{
+	public void PauseButton() {
+		if (Time.timeScale > 0) {
 			//Save our time scale to return to later, and then set to 0
 			saveTimeScale = Time.timeScale;
 			Time.timeScale = 0;
 		} 
-		else 
-		{
+		else {
 			//Return the timeScale to the original value
 			Time.timeScale = saveTimeScale;
 		}
 	}
-
-	public void FastForwardButton()
-	{ 
-		if (Time.timeScale < maxFFW)
-			Time.timeScale *= 2; //double unitl maxFFW reached
-		else
-			Time.timeScale = 1;
-	}	
+	public void FastForwardButton() {
+		//When TimeScale = 1, TimeScale will Scale to 2
+		//Depreciated comment, When TimeScale = 2, TimeScale will Scale to 4
+		//Any TimeScale >= 2, does not compute
+		//Change to Time.timeScale <= 2 to make maximum 4, currently allows objects to break through coliders on the membrane
+		if (Time.timeScale >= 1 && Time.timeScale < 2) {
+			Time.timeScale = Time.timeScale + Time.timeScale;
+		}
+		//Not user friendly TimeScale Modification
+		else if(Time.timeScale > 2) {
+			Time.timeScale = Time.timeScale / 2;		
+		}
+	}
 }
