144b5f3a51e20bffa41cea2bc4f2598be5355b72
J21TheSender <jrogers4@my.athens.edu>
2015-07-05 13:40:30 -0500

    Preliminary Tutorial Complete
    Completed the rough Draft of the Tutorial.

---
+++ Assets/Transparent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transparent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Color a = this.gameObject.GetComponent<Renderer> ().material.color;
+		// ().material.color;
+		a.a = 0.25f;
+		
+		this.gameObject.GetComponent<Renderer> ().material.color = a;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
